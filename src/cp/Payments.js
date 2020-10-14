a1688b0eb7cc77b878174c9de2da45867e0d5050
Tim Clark <tim.clark.nz@gmail.com>
2020-10-14 12:59:46 +0700

    format

--- src/cp/Payments.js
+++ src/cp/Payments.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import NumberFormat from 'react-number-format';
 
 export default class Payments extends Component {
 
@@ -18,9 +19,9 @@ export default class Payments extends Component {
     render() {
       
         return this.props.data.payment.map((p) => (
-            <div className="d-flex justify-content-between p-2">
+            <div className="d-flex justify-content-between p-2 shadow-lg p-3 mb-5 bg-white rounded">
                 <div>{this.get_name(p.studentid)}</div>
-                <div>{p.amount}</div>
+                <div><NumberFormat value={p.amount} displayType={'text'} thousandSeparator={true} /></div>
             </div>
         ))
     }
